00e3f8a6b9bdc77791bb0b0dd06023518388f3ca
19179166 <19179166@DSN1271158607.cfp127aluno.local>
2021-02-12 16:49:10 -0200

    feito pisca led no pino RD7

--- blink.X/main.c
+++ blink.X/main.c
@@ -7,8 +7,18 @@
 
 
 #include <xc.h>
+#include "config.h"
 
 void main(void) 
 {
-   
+    TRISDbits.TRISD7= 0;
+            
+    while(1)
+    {
+        PORTDbits.RD7= 1;
+        __delay_ms(500);
+        PORTDbits.RD7= 1;
+        
+    }
+    
 }
